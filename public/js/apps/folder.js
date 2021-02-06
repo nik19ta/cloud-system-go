4524f3cd5f127a14051d927b6709768b21a0a314
nik19ta <nikhvatov19@gmail.com>
2021-02-07 02:20:58 +0300

    add icons in folder

--- public/js/apps/folder.js
+++ public/js/apps/folder.js
@@ -24,24 +24,55 @@ fetch(URL + 'local_files', {
 
         for (let i = 0; i < JSON.parse(response).length; i++) {
             console.log(JSON.parse(response)[i]['Name']);
-            files = files + `<div>${JSON.parse(response)[i]['Name']}</div>`
+
+            files = files + `<div 
+            class='
+            ${JSON.parse(response)[i]['IsFolder'] ? "file_folder" : "file_no_folder"} 
+            ${i % 2 == 0 ? 'fg_line' : 'bg_line'} line' >
+            <img class='image' src="${JSON.parse(response)[i]['IsFolder'] ?'../../images/res/folder/folder.png' : '../../images/res/folder/file.png'}" alt="">
+            ${JSON.parse(response)[i]['Name']}
+            </div>`
             console.log('-------');
         }
 
         console.log(files);
 
         folder = new app('folder', 'folder.png', 600, 400, false, 'Проводник', `
-            <div class="app_folder" >${files}</div>
+            <div class="app_folder" ><div class="line back" >. .</div> ${files}</div>
                 
             <style>
             .app_folder{
-                padding: 5px;
+                width: 100%;
+                padding: 8px;
+                font-size: 12px;
                 color: #fff;
                 width: 600px;
                 height: calc(400px - 25px);
                 background-color: rgb(30, 30, 30);
                 border-bottom-left-radius: 8px;
                 border-bottom-right-radius: 8px;
+            }
+            .line{
+                display: flex;
+                align-items: center;
+                gap: 4px;
+                cursor: pointer;
+                width: 100%;
+                border-radius: 4px;
+                padding: 2px;
+            }
+            .bg_line{
+                background: rgb(41,41,41);
+            }
+            .fg_line{
+                
+            }
+            .image{
+                width: 16px;
+                height: 16px;
+            }
+            .back{
+                padding-left: 25px;
             }
             </style>
     `)
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,3 +1,3 @@
-let desktop_background = './images/backgrounds/2.jpg'
+let desktop_background = './images/backgrounds/1.jpg'
 let dock_position = 'down' // left/right/down
 let title_page = 'Your Settings'
