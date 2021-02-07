03c893250af3a78cbb7ab86ce2282c2d79ba3ff5
nik19ta <nikhvatov19@gmail.com>
2021-02-07 16:20:16 +0300

    поменял работу апи сервера

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nik19ta/go_server
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,43 +1,53 @@
 package main
- 
-import (
+
+import(
     "encoding/json"
     "fmt"
     "log"
     "net/http"
     "io/ioutil"
+    "github.com/gorilla/mux"
 )
- 
-type Localfile struct {
-    Name       string
-    IsFolder   bool
-}
- 
-func main() {
 
-    fs := http.FileServer(http.Dir("public"))
+type Localfile struct {
+    Name string
+    IsFolder bool
+}
+
+func getFiles(w http.ResponseWriter, r * http.Request) {
+    localfiles := [] Localfile {}
+
+    files, err := ioutil.ReadDir(".")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+        localfiles = append(localfiles, Localfile{file.Name(), file.IsDir()})
+    }
+
+    json_data2,
+    err := json.Marshal(localfiles)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(string(json_data2))
+}
+
+func main() {
+    fs := http.FileServer(http.Dir("./public"))
+
+    router := mux.NewRouter()
+
+    router.HandleFunc("/api/local_files", getFiles)
+
+    http.Handle("/api/", router)
 
     http.Handle("/", fs)
-
-    http.HandleFunc("/local_files", func(w http.ResponseWriter, r *http.Request){
-        localfiles := []Localfile{}
-
-        files, err := ioutil.ReadDir(".")
-        if err != nil { log.Fatal(err) }
-
-        for _, file := range files {
-            localfiles = append(localfiles, Localfile{file.Name(), file.IsDir()})
-        }
-
-        json_data2, err := json.Marshal(localfiles)
-        
-        if err != nil { log.Fatal(err) }
-
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusCreated)
-        json.NewEncoder(w).Encode(string(json_data2))
-    })
-
 
     fmt.Println("Server is listening...")
     http.ListenAndServe(":3000", nil)
