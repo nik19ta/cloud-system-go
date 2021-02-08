f2b95aca285fb289dd0eeab1182288a56186f741
nik19ta <nikhvatov19@gmail.com>
2021-02-08 04:49:30 +0300

    добавил приложение Терминал

---
+++ public/js/apps/terminal.js
@@ -0,0 +1,14 @@
+let terminal = new app('terminal', true, 'terminal.png', 600, 380, false, 'Terminal', `
+<div class="terminal" ></div>
+    
+<style>
+.terminal{
+    background-color: rgb(30, 30, 30);
+    width: 600px;
+    height: calc(380px - 25px);
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+</style>
+
+    `)
