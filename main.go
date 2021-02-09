7e97d1fab7a0011fe3c477dc63656b3eeb61355f
nik19ta <nikhvatov19@gmail.com>
2021-02-10 02:32:14 +0300

    Начал переписывать код

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -19,42 +16,21 @@ type Localfile struct {
 	Name     string
 	IsFolder bool
 }
-type Files struct {
-	Name string
-	Data string
-}
 
 func readfile(w http.ResponseWriter, r *http.Request) {
-	files := []Files{}
-
 	vars := mux.Vars(r)
-	file := vars["file"]
+	filename := vars["file"]
 
-	file = strings.Replace(file, "slash", "/", 20)
+	path := strings.Replace(filename, "slash", "/", 20)
 
-	f, err := os.Open(file)
+	file := wwf.RecordFile(path)
 
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	wr := bytes.Buffer{}
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		wr.WriteString(sc.Text())
-	}
-	files = append(files, Files{file, wr.String()})
-
-	json_data, err := json.Marshal(files)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	file.Open()
+	jsonfile, _ := file.Send()
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(string(json_data))
+	json.NewEncoder(w).Encode(string(jsonfile))
 
 }
 
@@ -84,19 +60,14 @@ func getFiles(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
 	fs := http.FileServer(http.Dir("./public"))
-
 	router := mux.NewRouter()
 
 	router.HandleFunc(`/api/local_files/dir="{dir}"`, getFiles)
-
 	router.HandleFunc(`/api/readfile/file="{file}"`, readfile)
-
 	router.HandleFunc(`/api/local_files`, getFiles)
 
 	http.Handle("/api/", router)
-
 	http.Handle("/", fs)
 
 	fmt.Println("Server is listening...")
