01658406b6147d2fdf463d00c10a5247328d4c92
nik19ta <nikhvatov19@gmail.com>
2021-02-12 01:28:18 +0300

    переделал folder

--- public/js/apps/folder.js
+++ public/js/apps/folder.js
@@ -1,5 +1,6 @@
 let files = ''
 let url_folder = '..'
+let lastcolor = ''
 
 const folder_html = `
     <div class="app_folder" >${files}</div>
@@ -17,27 +18,52 @@ const folder_html = `
             border-bottom-left-radius: 8px;
             border-bottom-right-radius: 8px;
             overflow-y: auto;
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: row;
+            gap: 20px;
         }
         .line{
             display: flex;
             align-items: center;
             gap: 4px;
             cursor: pointer;
-            width: 100%;
+            width: 80px;
+            height: 80px;
+            backdorund: red;
             border-radius: 4px;
             padding: 2px;
+            flex-wrap: wrap;
         }
         .bg_line{
-            background: rgb(41,41,41);
+            // background: rgb(41,41,41);
         }
         .fg_line{
-            
+            // background: rgb(41,41,41);
+        }
+        .folder__icon{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
         }
         .image{
-            width: 13px;
+            height: 50px;
         }
         .back{
             padding-left: 25px;
+        }
+        .folder__filename{
+            width: 100%;
+            font-size: 10px;
+            margin-top: 3px;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+        .center {
+            display: flex;
+            justify-content: center;
         }
     </style>
     `
@@ -51,7 +77,6 @@ let folder = new app('folder', true, 'folder.png', 600, 400, false, 'Прово�
 folder.folder_open_file = (filename) => {
     let localpath = url_folder + 'slash' + filename;
     folder.getfetch(`/api/readfile/file="${localpath}"`, (r) => {
-        // console.log(JSON.parse(r));
         reader.callback(JSON.parse(r)['Data'], JSON.parse(r)['Name'])
     })
 
@@ -68,7 +93,7 @@ folder.tofile = (dir) => {
 
     console.log('Запрос на', url_folder);
     folder.getfetch(`/api/local_files/dir="${url_folder}"`, (response) => {
-        files = `<div class="line back fg_line" onclick='folder.tofile("..")'>. .</div>`
+        files = `<div class="line back fg_line" ondblclick='folder.tofile("..")'>. .</div>`
 
         while (app_folder.firstChild) {
             app_folder.removeChild(app_folder.firstChild);
@@ -83,14 +108,22 @@ folder.tofile = (dir) => {
 
 folder.elem = (data, i) => {
     return ` <div 
-        class='${data['IsDirectory'] ? "file_folder" : "file_no_folder"}  ${i % 2 == 0 ? 'bg_line' : 'fg_line'} line'
-        ${data['IsDirectory'] ? `onclick='folder.tofile("${data['Name']}")'` : `onclick='folder.folder_open_file("${data['Name']}")'` } >
+        class='${data['IsDirectory'] ? "file_folder" : "file_no_folder"}  ${i % 2 == 0 ? 'bg_line' : 'fg_line'} line line_${data['Name'].replace(/\s/g, '')}'
 
-        <img class='image' src="${data['IsDirectory'] ?'../../images/res/folder/folder.png' : '../../images/res/folder/file.png'}" alt="">
+        ${data['IsDirectory'] ? `ondblclick='folder.tofile("${data['Name']}")'` : `ondblclick='folder.folder_open_file("${data['Name']}")'` }
+        ${`onclick='folder.set_active("${data['Name']}")'`}
+        
+        >
 
-        ${data['Name']}
+        <div class='folder__icon' >
+        <img class='image' src="${folder.setimg(data['Name'], data['IsDirectory'])}" alt="">
+        </div>
+
+        <p class="folder__filename ${data['Name'].length < 15 ? 'center' : ''} " >${data['Name']}</p>
+
         </div>`
-}
+    }
+    // ${data['Name']}
 folder.getfetch = (url, callback) => {
     fetch(`${url}`, {
             headers: {
@@ -105,4 +138,37 @@ folder.getfetch = (url, callback) => {
             callback(response)
         })
         .catch(err => console.log(err))
+}
+folder.set_active = (data) => {
+    if (document.querySelector(`.selected_line`) == null) {
+        document.querySelector(`.line_${data.replace(/\s/g, '')}`).className = document.querySelector(`.line_${data.replace(/\s/g, '')}`).className + " selected_line"
+        lastcolor = document.querySelector(`.line_${data.replace(/\s/g, '')}`).style.background;
+        document.querySelector(`.line_${data.replace(/\s/g, '')}`).style.background = "rgb(14, 91, 205)"
+    } else {
+        console.log(1);
+        document.querySelector(`.selected_line`).style.background = lastcolor
+        document.querySelector(`.selected_line`).classList.remove("selected_line")
+        lastcolor = document.querySelector(`.line_${data.replace(/\s/g, '')}`).style.background;
+        document.querySelector(`.line_${data.replace(/\s/g, '')}`).className = document.querySelector(`.line_${data.replace(/\s/g, '')}`).className + " selected_line"
+    }
+
+}
+folder.setimg = (name, type) => {
+    let path = '../../images/res/folder';
+    if (name.indexOf('.') !== -1) {
+        console.log(name.split('.')[name.split('.').length - 1]);
+
+        if (name.split('.')[name.split('.').length - 1] === 'zip') {
+            return `${path}/zip.png`
+        } else if (name.split('.')[name.split('.').length - 1] === 'go') {
+            return `${path}/golang.png`
+        }
+    }
+
+    if (type) {
+        return `${path}/folder.png`
+    } else {
+        return `${path}/file.png`
+    }
+
 }
